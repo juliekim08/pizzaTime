515ab6f111dd775efa03bdbbce3847dfd4d0bcc9
Your Name <julie.eun.kim@gmail.com  git config --global user.name juliekim08>
2015-08-24 23:46:16 -0500

    updated all files

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# pizzaTime
+It's PizzaTime is a prototype pizza franchise project, inspired by the Teenage Mutant Ninja Turtles. The was to create three locations with three different menus, without using bootstrap.
--- pizzaTime.js
+++ pizzaTime.js
@@ -1,5 +1,81 @@
 $(function() {
-    var city = '';
+    var pizzaOptions = {
+        "SanFrancisco": {
+            city: "San Francisco",
+            name: "Michaelangelo Says",
+            about: "San Francisco\'s location and home to the sourdough crust",
+            detailedLocation: "Greenwich and Fillmore, San Francisco, CA",
+            pizzas: [
+                {
+                    name: 'Meatballs on Meatballs on More Meatballs',
+                    toppings: 'Pepperoni Meatball, Chorizo Meatball, Turkey Meatball'
+                }, {
+                    name: 'You Don\'t Make Friends with Salad',
+                    toppings: 'Mushrooms'
+                }, {
+                    name: 'More Cheese Please',
+                    toppings: 'Mozaraella, Asiago, Pepperjack, Cheddar, Goat Cheese, Prolone'
+                }]
+        },
+        "Chicago": {
+            city: "Chicago",
+            name: "Leonardo\s Pizza Loft",
+            about: "Chicago is the first location and home of the deep dish pretzel crust.",
+            detailedLocation: "Division and Milwaukee, Chicago, IL",
+            pizzas: [
+                {
+                    name: 'Meat Mania',
+                    toppings: 'Hamburger, Steak, Bacon, Pepperoni and Ham'
+                }, {
+                    name: 'Veggie-tastic',
+                    toppings: 'Broccoli, Onions, Mushrooms and Red Pepper'
+                }, {
+                    name: 'Pretzel Pie',
+                    toppings: 'Cheddar and Mozaraella'
+                }]
+        },
+        "NewYork": {
+            city: "New York",
+            name: "Raphael's Refuge",
+            about: "New York location and home of the foccaccia",
+            detailedLocation: "E. 6th St and 2nd Ave , New York, NY",
+            pizzas: [
+                {
+                    name: 'You\'re So Fancy',
+                    toppings: 'Bacon, Pepperoni, Garlic and Truffle Oil'
+                }, {
+                    name: 'Hot Sauce On My Hot Sauce',
+                    toppings: 'Habanero, Siracha, Asparagus and Mushrooms'
+                }, {
+                    name: 'The Classic',
+                    toppings: 'Mozaraella and Basil'
+                }]
+        }
+
+    };
+
+    function populateToppings(pos) {
+        var index = 0;
+        $("#pizzaType option:selected").each(function(i, item) {
+            index = item.index;
+        });
+        var toppings = pos["pizzas"][index]["toppings"];
+        $("#pizza-description").html(toppings);
+    }
+
+    function populateCityAttributes(city) {
+        var pos = pizzaOptions[city], $pizzaType = $("#pizzaType");
+        $("#title").html(pos["name"]);
+        $("#city-detail-location").html(pos["detailedLocation"]);
+        $pizzaType.html("");
+        $.each(pos["pizzas"], function(idx, item) {
+            $pizzaType.append("<option value=" + idx +">" + item["name"] + "</option>");
+        });
+        $pizzaType.change(function() {
+            populateToppings(pos);
+        });
+        populateToppings(pos);
+    }
 
     $('.city-links li a').on('click', function() {
         $('a').removeClass('active');
@@ -7,98 +83,24 @@ $(function() {
             $(this).addClass('active');
         }
         $('.banner').slideUp();
-        window.city = $(this).attr('href');
-
-        if (window.city != "") {
-            var newCity = $('div.city-content').find(window.city);
+        var city = $(this).attr("href").substring(1);
+        if (city) {
+            $("#default").addClass("hidden");
+            $("#city-template").removeClass("hidden");
+            populateCityAttributes(city);
+        } else {
+            $("#city-template").addClass("hidden");
+            $("#default").removeClass("hidden");
         }
-        newCity.show().siblings().hide();
-        newCity.siblings().addClass('hidden');
-        newCity.removeClass('hidden');
-        return window.city;
     });
 
-     $('.header').on('click',function(){
+    $('.header').on('click',function(){
         $('.city-content > div').addClass('hidden').hide();
         $('.banner').slideDown();
     });
 
-  //    var pizzaOptions = [
-  //   {
-  //     name: 'Meatballs on Meatballs on More Meatballs',
-  //     toppings: 'Pepperoni Meatball, Chorizo Meatball, Turkey Meatball'
-  //   },
-  //   {
-  //     name: 'You Don\'t Make Friends with Salad',
-  //     toppings: 'Mushrooms'
-  //   },
-  //   {
-  //     name: 'More Cheese Please',
-  //     toppings: 'Mozaraella, Asiago, Pepperjack, Cheddar, Goat Cheese, Prolone'
-  //   }
-  // ]
-  // for (var i = 0; i < pizzaOptions.length; i++){
-  //   $('#pizzaType').append('<option value="' + i + '">' + pizzaOptions[i]['name']+'</option>');
-//   $('.pizzaDisplay').append('<option value="' + i + '">' + pizzaOptions[i]['name']+'</option>')
-  // }
+    if (window.location.hash.substring(1)) {
+        populateCityAttributes(window.location.hash.substring(1));
+    }
 
 });
-    //Future Feature: Easy City Add for New Franchase. In Testing for Q4
-    // var cityArray = ["San Francisco", "Chicago", "New York"];
-    // //To add a list of the new Franchise city, enter the city name between the "" and uncomment the link below.
-    // // cityArray.push("Enter new City Here");
-    // for (var i = 0; i < cityArray.length; i++) {
-    // $('.city-links').append('<li><a href="#' + cityArray[i] +'">'+cityArray[i]+'</a></li>');
-    // }
-    //Future Feature: Online Order. Still in progress for Q3 Release
-    // var price = '';
-    // if ($('#pizzaType').val() == 'pizza1') {
-    //     price = 10;
-    // } else if ($('#pizzaType').val() == 'pizza2') {
-    //     price = 12;
-    // } else if ($('#pizzaType').val() == 'pizza3') {
-    //     price = 14;
-    // }
-    // var qty = '';
-    // if ($('#pizzaQty').val() == '1') {
-    //     qty = 1;
-    // } else if ($('#pizzaQty').val() == '2') {
-    //     qty = 2;
-    // } else if ($('#pizzaQty').val() == '3') {
-    //     qty = 3;
-    // }
-    // var pizzaTotal = price * qty;
-    // console.log(pizzaTotal);
-
-    //For Versioning Control Purposes Only. Code was adjusted on 8/4/2015
-
-    // $('.city-links li a').on('click', function() {
-    //     $('a').removeClass('active');
-    //     if ($(this).hasClass('active') === false) {
-    //         $(this).addClass('active');
-    //     }
-    //     $('.banner').slideUp();
-    //     window.city = $(this).attr('href');
-    //     if (window.city == '#SanFrancisco') {
-    //         $('#SanFrancisco').show().siblings().hide();
-    //         $('#SanFrancisco').siblings().addClass('hidden');
-    //         $('#SanFrancisco').removeClass('hidden');
-    //     } else if (window.city == '#Chicago') {
-    //         $('#Chicago').show().siblings().hide();
-    //         $('#Chicago').siblings().addClass('hidden');
-    //         $('#Chicago').removeClass('hidden');
-    //     } else if (window.city == '#NewYork') {
-    //         $('#NewYork').show().siblings().hide();
-    //         $('#NewYork').siblings().addClass('hidden');
-    //         $('#NewYork').removeClass('hidden');
-    //     }
-    //     return window.city;
-    // });
-
-
-
-
-
-
-
-
